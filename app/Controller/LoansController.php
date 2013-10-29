1c2a22232d7a74eb43ea63385c1a6c5f63da3ad4
Matthieu Clin <matthieu.clin@outlook.com>
2013-10-29 16:17:36 +0100

    Mise en place de la recherche d'une salle pour la demander par la suite

--- app/Controller/LoansController.php
+++ app/Controller/LoansController.php
@@ -1,6 +1,11 @@
 <?php 
 class LoansController extends AppController{
 
+	public $components = array('Paginator');
+
+	public $paginate = array(
+	    'limit' => 10
+	);
 
 	public function manager_answer() {
 		$this->layout = 'manager';
@@ -16,6 +21,32 @@ class LoansController extends AppController{
 		}
 		$res = $this->Loan->find('all', array('conditions' => array('Room.department_id' =>$this->Auth->User('department_id'),'Loan.date' => $date)));
 		$this->set('res', $res);
+	}
+
+
+	public function manager_ask(){
+		$this->layout = 'manager';
+		$this->Paginator->settings = $this->paginate;
+
+		// Retransmettre les variables dans le formulaire
+		$this->request->data['Room']= $this->request->query;
+
+		$conditions = $this->postConditions($this->request->data);
+
+		// Enleve le département de l'utilisateur des choix
+		if(empty($conditions) || !isset($conditions['Room.department_id'])
+							  || empty($conditions['Room.department_id'])){
+			unset($conditions['Room.department_id']);
+			$conditions['Room.department_id <>'] = $this->Auth->User('department_id');
+		}
+
+		$res = $this->Paginator->paginate('Room', $conditions);
+		$this->set('res', $res);
+		
+		// Pour le menu déroulant du formulaire
+		$listDepartment = $this->Loan->Department->find('list', array('conditions' => array('id <>' => $this->Auth->User('department_id'))));
+		$this->set('listDepartment', $listDepartment);
+
 	}
 
 
