7ffc321c7f370baf96af99d364c3785591803ac3
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-11-17 11:26:56 +0100

    Enregistrement des demandes dans la bdd

--- app/Model/Loan.php
+++ app/Model/Loan.php
@@ -2,5 +2,21 @@
 class Loan extends AppModel{
 
 	public $belongsTo = array('Department', 'Room', 'Status');
+
+
+	public function beforeSave($options = array()) {
+
+	    if (!empty($this->data['Loan']['date'])){
+	        $this->data['Loan']['date'] = $this->dateFormatBeforeSave($this->data['Loan']['date']);
+	        $this->data['Loan']['start_time'] = $this->data['Loan']['start_time'].':00';
+	        $this->data['Loan']['end_time'] = $this->data['Loan']['end_time'].':00';
+
+	    }
+	    return true;
+	}
+
+	public function dateFormatBeforeSave($dateString) {
+	   return date('Y-m-d', strtotime($dateString));
+	}
 }
  ?>
