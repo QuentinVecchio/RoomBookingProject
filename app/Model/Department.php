dab8d310d4a1ad4fb440b354c1ae94253cfac87a
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-12-09 22:43:48 +0100

    test angularjs et blindage departement

--- app/Model/Department.php
+++ app/Model/Department.php
@@ -1,5 +1,11 @@
 <?php 
 class Department extends AppModel{
 	public $hasMany = array('Room' =>array('order'=>'name'));
+
+	public $validate = array(
+			'name' => array(
+				'rule' => 'isUnique',
+				'message' => 'Nom déjà utilisé')
+		);
 }
  ?>
