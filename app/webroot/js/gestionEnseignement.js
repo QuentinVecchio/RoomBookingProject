1fa22fdde6847aad2fff119743151efe9ccf2841
Quentin VECCHIO <vecchioquentin@hotmail.fr>
2014-01-09 17:37:50 +0100

    Mise en forme gestion de formation

--- app/webroot/js/gestionEnseignement.js
+++ app/webroot/js/gestionEnseignement.js
@@ -2,6 +2,35 @@ var gestionFormation = angular.module('gestionFormation', []);
  
 gestionFormation.controller('gestionCtrl', function gestionCtrl($scope, $http) {
 
+	$scope.changement = function(idFormation){
+		alert('test');
+		if($scope.valeur[id] == true)
+		{
+			$scope.add(idFormation);
+		}
+		else
+		{
+			$scope.delete(idFormation);
+		}
+	}
 
+	$scope.add = function(idFormation){
+		/*$http.get($scope.urlAdd+'/'+idFormation).success(function(response) {
+				if(response == 0)
+				{
+					alert('Erreur lors de l\'ajout');
+				}
+		});	*/
+		alert("case cochée");
+	}
 
+	$scope.delete = function(idFormation){
+		/*$http.get($scope.urlDelete+'/'+idFormation).success(function(response) {
+				if(response == 0)
+				{
+					alert('Erreur lors de l\'ajout');
+				}
+		});*/	
+		alert("case décochée");
+	}
 });
